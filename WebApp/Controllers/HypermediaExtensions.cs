2b9f54b0320d00ddc139372acd6e25bf48b9c98d
Matthew Winder <@mwinder>
2015-05-20 10:32:20 +0100

    Href extension return Uri

--- WebApp/Controllers/HypermediaExtensions.cs
+++ WebApp/Controllers/HypermediaExtensions.cs
@@ -1,12 +1,13 @@
+using System;
 using System.Web.Http.Routing;
 
 namespace WebApp.Controllers
 {
     public static class HypermediaExtensions
     {
-        public static string Href(this UrlHelper url, object routeValues)
+        public static Uri Href(this UrlHelper url, object routeValues)
         {
-            return url.Link("DefaultApi", routeValues);
+            return new Uri(url.Link("DefaultApi", routeValues));
         }
     }
 }
