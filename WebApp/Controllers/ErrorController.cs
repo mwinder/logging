5c98439f33f089ed98b65cd5b7b392fd48e11132
Matthew Winder <@mwinder>
2015-05-20 10:37:32 +0100

    Added ErrorController.

---
+++ WebApp/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class ErrorController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            throw new Exception("Example error");
+        }
+    }
+}
