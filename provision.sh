0ea081ee03d5344bd5863c6b394a2838efb96ea0
Matthew Winder <@mwinder>
2015-06-25 14:29:25 +0100

    Kibana service script

--- provision.sh
+++ provision.sh
@@ -5,12 +5,17 @@ echo "deb http://packages.elastic.co/elasticsearch/1.6/debian stable main" | sud
 sudo apt-get update
 
 sudo apt-get install openjdk-7-jre-headless --yes
+
 sudo apt-get install elasticsearch --yes
 
-sudo update-rc.d elasticsearch defaults 95 10
-sudo /etc/init.d/elasticsearch start
-
 curl -O https://download.elastic.co/kibana/kibana/kibana-4.1.0-linux-x64.tar.gz
-sudo mkdir -p /srv/kibana
-sudo tar -C /srv/kibana -zxf kibana-4.1.0-linux-x64.tar.gz --strip 1
-/srv/kibana/bin/kibana
+sudo mkdir -p /opt/kibana
+sudo tar -C /opt/kibana -zxf kibana-4.1.0-linux-x64.tar.gz --strip 1
+sudo cp /vagrant/kibana /etc/init.d/kibana
+sudo chmod +x /etc/init.d/kibana
+
+sudo update-rc.d elasticsearch defaults 95 10
+sudo service elasticsearch start
+
+sudo update-rc.d kibana defaults 99 10
+sudo service kibana start
